b22a7c52ebc47ec847a01cf3c2ff5b5559698b12
HLFrye <HLFrye@github>
2018-12-06 13:06:04 -0600

    Day6 puzzle 2 completE

--- Day6/python/main.py
+++ Day6/python/main.py
@@ -1,18 +1,6 @@
 def distance(pt1, pt2):
   return abs(pt1[0] - pt2[0]) + abs(pt1[1] - pt2[1])
 
-def find_closest(pt, coords):
-  closest_coord = None
-  closest_coord_dist = 100000000
-  for coord in coords:
-    dist = distance(pt, coord)
-    if dist < closest_coord_dist:
-      closest_coord = coord
-      closest_coord_dist = dist
-      continue
-    if dist == closest_coord_dist:
-      closest_coord = None
-  return closest_coord
 
 def main():
   coords = []
@@ -28,31 +16,17 @@ def main():
 
       coords.append((int(parts[0]), int(parts[1])))
 
-  grid = [[0] * size[0] for i in range(size[1])]
+  safe_spaces = 0
 
-  coord_area = {}
-  infinite_coords = set()
+  for x in range(0, size[0]):
+    for y in range(0, size[1]):
+      total_dist = 0
+      for coord in coords:
+        total_dist = total_dist + distance((x, y), coord)
+      if total_dist < 10000:
+        safe_spaces = safe_spaces + 1
 
-  for y, line in enumerate(grid):
-    for x, _ in enumerate(line):
-      closest = find_closest((x,y), coords)
-      if closest is not None:
-        if closest in coord_area:
-          coord_area[closest] = coord_area[closest] + 1
-        else:
-          coord_area[closest] = 1
-        if x == 0 or y == 0 or x == size[0] - 1 or y == size[1] - 1:
-          infinite_coords.add(closest)
-
-  print("There are {} input coords, and {} are infinite".format(len(coords), len(infinite_coords)))
-
-  max_size = 0
-  for key, value in coord_area.items():
-    if key not in infinite_coords:
-      if value > max_size:
-        max_size = value
-    
-  print("The largest area is: {}".format(max_size))
+  print("The safe area is: {}".format(safe_spaces))
 
 if __name__ == '__main__':
   main()
