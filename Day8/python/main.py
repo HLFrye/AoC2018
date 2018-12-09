c8edcee8dbf4ef4929a9a73b2734031224062e16
HLFrye <HLFrye@github>
2018-12-08 21:02:47 -0600

    Day 8 puzzle 1 complete

---
+++ Day8/python/main.py
@@ -0,0 +1,26 @@
+def get_meta_sum(input_items):
+  num_children = input_items[0]
+  num_meta = input_items[1]
+  total_read = 2
+  total = 0
+  for child in range(0, num_children):
+    child_sum, child_num_read = get_meta_sum(input_items[total_read::])
+    total = total + child_sum
+    total_read = total_read + child_num_read
+  meta_start = input_items[total_read::]
+  for meta in range(0, num_meta):
+    total = total + meta_start[meta]
+    total_read = total_read + 1
+  return total, total_read
+
+def main():
+  with open("../input.txt") as input:
+    buf = list(map(lambda x: int(x), input.read().split()))
+  
+  total, num_read = get_meta_sum(buf)
+
+  print(total)
+
+
+if __name__ == "__main__":
+  main()
