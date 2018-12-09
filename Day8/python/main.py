6808031f43b3b3745e902dfa4c703675ce2b1b98
HLFrye <HLFrye@github>
2018-12-08 21:38:48 -0600

    Day 8 puzzle 2 complete

--- Day8/python/main.py
+++ Day8/python/main.py
@@ -2,15 +2,25 @@ def get_meta_sum(input_items):
   num_children = input_items[0]
   num_meta = input_items[1]
   total_read = 2
-  total = 0
+
+  child_amounts = []
+
   for child in range(0, num_children):
     child_sum, child_num_read = get_meta_sum(input_items[total_read::])
-    total = total + child_sum
+    child_amounts.append(child_sum)
     total_read = total_read + child_num_read
   meta_start = input_items[total_read::]
+
+  total = 0
   for meta in range(0, num_meta):
-    total = total + meta_start[meta]
+    meta_value = meta_start[meta]
+    if num_children == 0:
+      total = total + meta_value
+    else:
+      if (meta_value - 1) < len(child_amounts):
+        total = total + child_amounts[meta_value - 1]
     total_read = total_read + 1
+
   return total, total_read
 
 def main():
